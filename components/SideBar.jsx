54d1345ad0edae28f62b85aca162a087453ac2aa
Kushal <vankadara2004@gmail.com>
2025-04-29 19:48:06 +0530

    Project updated

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -7,7 +7,7 @@ import ChatLabel from './ChatLabel';
 
 const SideBar = ({expand, setExpand}) => {
   const {openSignIn}=useClerk();
-  const {user}=useAppContext();
+  const {user, chats, createNewChat}=useAppContext();
   const [openMenu, setOpenMenu]=useState({id: 0, open: false});
 
   return (
@@ -29,7 +29,7 @@ const SideBar = ({expand, setExpand}) => {
           </div>
         </div>
 
-        <button className={`mt-8 flex items-center justify-center cursor-pointer ${expand ? 'bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max' : 'group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg'}`}>
+        <button onClick={createNewChat} className={`mt-8 flex items-center justify-center cursor-pointer ${expand ? 'bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max' : 'group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg'}`}>
           <Image className={expand ? 'w-6' : 'w-7'} src={expand ? assets.chat_icon : assets.chat_icon_dull} alt=''/>
           <div className='absolute w-max -top-12 -right-12 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none'>
             New Chat
@@ -42,7 +42,11 @@ const SideBar = ({expand, setExpand}) => {
 
         <div className={`mt-8 text-white/25 text-sm ${expand ? 'block' : 'hidden'}`}>
           <p className='my-1'>Recents</p>
-          <ChatLabel openMenu={openMenu} setOpenMenu={setOpenMenu}/>
+          {
+            chats.map((chat, idx)=> (
+              <ChatLabel key={idx} openMenu={openMenu} setOpenMenu={setOpenMenu} id={chat._id} name={chat.name}/>
+            ))
+          }
         </div>
       </div>
 
