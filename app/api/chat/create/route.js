868e71756b69db00e06c78019509e9fbe10ab843
Kushal <vankadara2004@gmail.com>
2025-04-29 20:10:31 +0530

    Have to check in Future

--- app/api/chat/create/route.js
+++ app/api/chat/create/route.js
@@ -1,11 +1,12 @@
 import connectDB from "@/config/connectDB";
 import chatModel from "@/models/Chat";
-import { getAuth } from "@clerk/nextjs/server";
+import { auth } from '@clerk/nextjs/server';
 import { NextResponse } from "next/server";
 
 export async function POST(req){
   try{
-    const {userId}=getAuth(req);
+    const {userId}=auth(req);
+
     if(!userId){
       return NextResponse.json({
         success: false,
